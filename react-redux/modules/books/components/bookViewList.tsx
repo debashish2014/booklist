89542ddf3460179d37158524f2d75b030302c12d
Adam Rackis <adamrackis@hotmail.com>
2018-07-15 12:50:31 -0500

    Demos updated

--- react-redux/modules/books/components/bookViewList.tsx
+++ react-redux/modules/books/components/bookViewList.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
-const LOAD_BOOKS = `query getAllBooks($title:String) {
-  allBooks(title_contains:$title){
+const LOAD_BOOKS = compress`query getAllBooks($title:String, $page: Int) {
+  allBooks(title_contains:$title, PAGE: $page, PAGE_SIZE: 3){
     Books{
       _id,
       title
@@ -50,25 +50,38 @@ const hardResetStrategy = name => ({
   run: (args, resp, { hardReset }) => hardReset()
 });
 
-import { GraphQL, buildQuery, buildMutation } from "micro-graphql-react";
+import { Client, setDefaultClient, GraphQL, buildQuery, buildMutation, compress, Cache } from "micro-graphql-react";
+
+let c = new Cache(1);
+
+const graphqlClient = new Client({
+  endpoint: "/graphql",
+  fetchOptions: { credentials: "include" },
+  cacheSize: 2
+});
+
+setDefaultClient(graphqlClient);
 
 export default class BookViewingList extends Component<any, any> {
-  state = { titleSearch: "", editingBook: null };
+  state = { titleSearch: "", page: 1, editingBook: null };
   el: any;
   editBook = book => {
     this.setState({ editingBook: book });
   };
   render() {
+    let { page, titleSearch } = this.state;
     return (
       <div style={{ padding: "30px" }}>
         <input ref={el => (this.el = el)} />
         <button onClick={() => this.setState({ titleSearch: this.el.value })}>Update</button>
+        {this.state.page > 1 ? <button onClick={() => this.setState({ page: this.state.page - 1 })}>Prev</button> : null}
+        <button onClick={() => this.setState({ page: this.state.page + 1 })}>Next</button>
         <br />
         <br />
 
         <GraphQL
           query={{
-            loadBooks: buildQuery(LOAD_BOOKS, { title: this.state.titleSearch }, { onMutation: hardResetStrategy("Book") })
+            loadBooks: buildQuery(LOAD_BOOKS, { title: titleSearch, page }, { onMutation: hardResetStrategy("Book"), cache: c })
           }}
           mutation={{ updateBook: buildMutation(UPDATE_BOOK) }}
         >
